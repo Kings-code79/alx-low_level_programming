2facc1fb2e2bd56f8fcaad2c32de1df8fa982940
Kings-code79 <111255817+Kings-code79@users.noreply.github.com>
2022-09-08 17:19:16 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }
+
