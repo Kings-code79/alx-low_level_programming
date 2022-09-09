22df862109e9cb9415e5ae91bbc1e9ef280908d4
Kings-code79 <kingsleyobott@gmail.com>
2022-09-09 04:54:24 -0700

    commit for insopmnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - prints alphabet in lowercse
+ *
+ *  Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
+
