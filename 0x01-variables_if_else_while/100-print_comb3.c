27589f1b2d9a1e6209f43c6e6c24a5955c9668fa
Kings-code79 <kingsleyobott@gmail.com>
2022-09-09 06:38:59 -0700

    commit for task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints possible combinations of two digit numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
