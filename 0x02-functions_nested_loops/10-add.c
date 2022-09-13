8af373114b52663fd4c652d2ad88a3d8027343fb
Kings-code79 <kingsleyobott@gmail.com>
2022-09-13 07:39:23 -0700

    commit for task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add 2 integers
+ * @num1: ist numbver
+ * @num2: 2nd number
+ *
+ * Return: returnsthe addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
