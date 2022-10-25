4800f8fc0543697bd72241b918d33103c9c85494
Kings-code79 <111255817+Kings-code79@users.noreply.github.com>
2022-10-25 13:47:48 +0100

    first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
   * print_listint - prints all elements of a listint_t list.
-  * @h: pointer to first element in a list.
+  * @h: pointer to first element in a lists.
   *
   * Return:  number of nodes.
   */
